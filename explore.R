39326dc95ed531b3fb3f2e94f24bf81ff8e068ac
KitaTanya <69657536+KitaTanya@users.noreply.github.com>
2021-02-07 02:27:40 -0500

    variables

--- explore.R
+++ explore.R
@@ -3,6 +3,13 @@ library(dplyr)
 library(ggplot2)
 
 #---- Load Data
-load("brfss2013.Rdata")
+load("data/brfss2013.Rdata")
 
-brfss2013.Rdata
+head(brfss2013)
+
+females <- filter(brfss2013, sex == 'Female')
+
+colnames(females)
+
+
+group = subset(females, select=c())
